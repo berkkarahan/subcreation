919aea3b15c13272d81990247eeec7bc06f65e6e
Aveline Estié <aa.estie@gmail.com>
2022-12-15 13:42:45 +0100

    should be all enchants!

--- enchants.py
+++ enchants.py
@@ -1,23 +1,63 @@
 enchant_mapping = {}
 # Dragonflight
-# Back
-enchant_mapping["6587"] = {"id": 199950, "name": "Writ of Avoidance", "quality": 1}
-enchant_mapping["6590"] = {"id": 199947, "name": "Graceful Avoidance", "quality": 1}
-enchant_mapping["6591"] = {"id": 199989, "name": "Graceful Avoidance", "quality": 2}
-enchant_mapping["6593"] = {"id": 199951, "name": "Writ of Leech", "quality": 1}
-enchant_mapping["6594"] = {"id": 199993, "name": "Writ of Leech", "quality": 2}
-enchant_mapping["6595"] = {"id": 200035, "name": "Writ of Leech", "quality": 3}
-enchant_mapping["6596"] = {"id": 199949, "name": "Regenerative Leech", "quality": 1}
-enchant_mapping["6597"] = {"id": 199991, "name": "Regenerative Leech", "quality": 2}
-enchant_mapping["6598"] = {"id": 200033, "name": "Regenerative Leech", "quality": 3}
+# Leg armor
+enchant_mapping["6488"] = {"id": 193557, "name": "Fierce Armor Kit", "quality": 1}
+enchant_mapping["6489"] = {"id": 193561, "name": "Fierce Armor Kit", "quality": 2}
+enchant_mapping["6490"] = {"id": 193565, "name": "Fierce Armor Kit", "quality": 3}
+enchant_mapping["6491"] = {"id": 193559, "name": "Reinforced Armor Kit", "quality": 1}
+enchant_mapping["6492"] = {"id": 193563, "name": "Reinforced Armor Kit", "quality": 2}
+enchant_mapping["6493"] = {"id": 193567, "name": "Reinforced Armor Kit", "quality": 3}
+enchant_mapping["6494"] = {"id": 193556, "name": "Frosted Armor Kit", "quality": 1}
+enchant_mapping["6495"] = {"id": 193560, "name": "Frosted Armor Kit", "quality": 2}
+enchant_mapping["6496"] = {"id": 193564, "name": "Frosted Armor Kit", "quality": 3}
 
-# Chest
-enchant_mapping["6614"] = {"id": 199943, "name": "Accelerated Agility", "quality": 1}
-enchant_mapping["6617"] = {"id": 199944, "name": "Reserve of Intellect", "quality": 1}
-enchant_mapping["6618"] = {"id": 199986, "name": "Reserve of Intellect", "quality": 2}
-enchant_mapping["6623"] = {"id": 199946, "name": "Waking Stats", "quality": 1}
-enchant_mapping["6624"] = {"id": 199988, "name": "Waking Stats", "quality": 2}
-enchant_mapping["6625"] = {"id": 200030, "name": "Waking Stats", "quality": 3}
+# Scope
+enchant_mapping["6520"] = {"id": 198310, "name": "Gyroscopic Kaleidoscope", "quality": 1}
+enchant_mapping["6521"] = {"id": 198311, "name": "Gyroscopic Kaleidoscope", "quality": 2}
+enchant_mapping["6522"] = {"id": 198312, "name": "Gyroscopic Kaleidoscope", "quality": 3}
+enchant_mapping["6523"] = {"id": 198313, "name": "Projectile Propulsion Pinion", "quality": 1}
+enchant_mapping["6524"] = {"id": 198314, "name": "Projectile Propulsion Pinion", "quality": 2}
+enchant_mapping["6525"] = {"id": 198315, "name": "Projectile Propulsion Pinion", "quality": 3}
+enchant_mapping["6526"] = {"id": 198316, "name": "High Intensity Thermal Scanner", "quality": 1}
+enchant_mapping["6527"] = {"id": 198317, "name": "High Intensity Thermal Scanner", "quality": 2}
+enchant_mapping["6528"] = {"id": 198318, "name": "High Intensity Thermal Scanner", "quality": 3}
+
+# Spellthread
+enchant_mapping["6536"] = {"id": 194008, "name": "Vibrant Spellthread", "quality": 1}
+enchant_mapping["6537"] = {"id": 194009, "name": "Vibrant Spellthread", "quality": 2}
+enchant_mapping["6538"] = {"id": 194010, "name": "Vibrant Spellthread", "quality": 3}
+enchant_mapping["6539"] = {"id": 194011, "name": "Frozen Spellthread", "quality": 1}
+enchant_mapping["6540"] = {"id": 194012, "name": "Frozen Spellthread", "quality": 2}
+enchant_mapping["6541"] = {"id": 194013, "name": "Frozen Spellthread", "quality": 3}
+enchant_mapping["6542"] = {"id": 194014, "name": "Temporal Spellthread", "quality": 1}
+enchant_mapping["6543"] = {"id": 194015, "name": "Temporal Spellthread", "quality": 2}
+enchant_mapping["6544"] = {"id": 194016, "name": "Temporal Spellthread", "quality": 3}
+
+# Ring
+enchant_mapping["6545"] = {"id": 199957, "name": "Writ of Critical Strike", "quality": 1}
+enchant_mapping["6546"] = {"id": 199999, "name": "Writ of Critical Strike", "quality": 2}
+enchant_mapping["6547"] = {"id": 200041, "name": "Writ of Critical Strike", "quality": 3}
+enchant_mapping["6548"] = {"id": 199953, "name": "Devotion of Critical Strike", "quality": 1}
+enchant_mapping["6549"] = {"id": 199995, "name": "Devotion of Critical Strike", "quality": 2}
+enchant_mapping["6550"] = {"id": 200037, "name": "Devotion of Critical Strike", "quality": 3}
+enchant_mapping["6551"] = {"id": 199958, "name": "Writ of Haste", "quality": 1}
+enchant_mapping["6552"] = {"id": 200000, "name": "Writ of Haste", "quality": 2}
+enchant_mapping["6553"] = {"id": 200042, "name": "Writ of Haste", "quality": 3}
+enchant_mapping["6554"] = {"id": 199954, "name": "Devotion of Haste", "quality": 1}
+enchant_mapping["6555"] = {"id": 199996, "name": "Devotion of Haste", "quality": 2}
+enchant_mapping["6556"] = {"id": 200038, "name": "Devotion of Haste", "quality": 3}
+enchant_mapping["6557"] = {"id": 199959, "name": "Writ of Mastery", "quality": 1}
+enchant_mapping["6558"] = {"id": 200001, "name": "Writ of Mastery", "quality": 2}
+enchant_mapping["6559"] = {"id": 200043, "name": "Writ of Mastery", "quality": 3}
+enchant_mapping["6560"] = {"id": 199955, "name": "Devotion of Mastery", "quality": 1}
+enchant_mapping["6561"] = {"id": 199997, "name": "Devotion of Mastery", "quality": 2}
+enchant_mapping["6562"] = {"id": 200039, "name": "Devotion of Mastery", "quality": 3}
+enchant_mapping["6563"] = {"id": 199960, "name": "Writ of Versatility", "quality": 1}
+enchant_mapping["6564"] = {"id": 200002, "name": "Writ of Versatility", "quality": 2}
+enchant_mapping["6565"] = {"id": 200044, "name": "Writ of Versatility", "quality": 3}
+enchant_mapping["6566"] = {"id": 199956, "name": "Devotion of Versatility", "quality": 1}
+enchant_mapping["6567"] = {"id": 199998, "name": "Devotion of Versatility", "quality": 2}
+enchant_mapping["6568"] = {"id": 200040, "name": "Devotion of Versatility", "quality": 3}
 
 # Wrist
 enchant_mapping["6569"] = {"id": 199940, "name": "Writ of Avoidance", "quality": 1}
@@ -39,67 +79,109 @@ enchant_mapping["6584"] = {"id": 199939, "name": "Devotion of Speed", "quality":
 enchant_mapping["6585"] = {"id": 199981, "name": "Devotion of Speed", "quality": 2}
 enchant_mapping["6586"] = {"id": 200023, "name": "Devotion of Speed", "quality": 3}
 
+# Back
+enchant_mapping["6587"] = {"id": 199950, "name": "Writ of Avoidance", "quality": 1}
+enchant_mapping["6588"] = {"id": 199992, "name": "Writ of Avoidance", "quality": 2}
+enchant_mapping["6589"] = {"id": 200034, "name": "Writ of Avoidance", "quality": 3}
+enchant_mapping["6590"] = {"id": 199947, "name": "Graceful Avoidance", "quality": 1}
+enchant_mapping["6591"] = {"id": 199989, "name": "Graceful Avoidance", "quality": 2}
+enchant_mapping["6592"] = {"id": 200031, "name": "Graceful Avoidance", "quality": 3}
+enchant_mapping["6593"] = {"id": 199951, "name": "Writ of Leech", "quality": 1}
+enchant_mapping["6594"] = {"id": 199993, "name": "Writ of Leech", "quality": 2}
+enchant_mapping["6595"] = {"id": 200035, "name": "Writ of Leech", "quality": 3}
+enchant_mapping["6596"] = {"id": 199949, "name": "Regenerative Leech", "quality": 1}
+enchant_mapping["6597"] = {"id": 199991, "name": "Regenerative Leech", "quality": 2}
+enchant_mapping["6598"] = {"id": 200033, "name": "Regenerative Leech", "quality": 3}
+enchant_mapping["6599"] = {"id": 199952, "name": "Writ of Speed", "quality": 1}
+enchant_mapping["6600"] = {"id": 199994, "name": "Writ of Speed", "quality": 2}
+enchant_mapping["6601"] = {"id": 200036, "name": "Writ of Speed", "quality": 3}
+enchant_mapping["6602"] = {"id": 199948, "name": "Homebound Speed", "quality": 1}
+enchant_mapping["6603"] = {"id": 199990, "name": "Homebound Speed", "quality": 2}
+enchant_mapping["6604"] = {"id": 200032, "name": "Homebound Speed", "quality": 3}
+
+# Boots
+enchant_mapping["6605"] = {"id": 199934, "name": "Plainsrunner's Breeze", "quality": 1}
+enchant_mapping["6606"] = {"id": 199976, "name": "Plainsrunner's Breeze", "quality": 2}
+enchant_mapping["6607"] = {"id": 200018, "name": "Plainsrunner's Breeze", "quality": 3}
+enchant_mapping["6608"] = {"id": 199935, "name": "Rider's Reassurance", "quality": 1}
+enchant_mapping["6609"] = {"id": 199977, "name": "Rider's Reassurance", "quality": 2}
+enchant_mapping["6610"] = {"id": 200019, "name": "Rider's Reassurance", "quality": 3}
+enchant_mapping["6611"] = {"id": 199936, "name": "Watcher's Loam", "quality": 1}
+enchant_mapping["6612"] = {"id": 199978, "name": "Watcher's Loam", "quality": 2}
+enchant_mapping["6613"] = {"id": 200020, "name": "Watcher's Loam", "quality": 3}
+
+# Chest
+enchant_mapping["6614"] = {"id": 199943, "name": "Accelerated Agility", "quality": 1}
+enchant_mapping["6615"] = {"id": 199985, "name": "Accelerated Agility", "quality": 2}
+enchant_mapping["6616"] = {"id": 200027, "name": "Accelerated Agility", "quality": 3}
+enchant_mapping["6617"] = {"id": 199944, "name": "Reserve of Intellect", "quality": 1}
+enchant_mapping["6618"] = {"id": 199986, "name": "Reserve of Intellect", "quality": 2}
+enchant_mapping["6619"] = {"id": 200028, "name": "Reserve of Intellect", "quality": 3}
+enchant_mapping["6620"] = {"id": 199945, "name": "Sustained Strength", "quality": 1}
+enchant_mapping["6621"] = {"id": 199987, "name": "Sustained Strength", "quality": 2}
+enchant_mapping["6622"] = {"id": 200029, "name": "Sustained Strength", "quality": 3}
+enchant_mapping["6623"] = {"id": 199946, "name": "Waking Stats", "quality": 1}
+enchant_mapping["6624"] = {"id": 199988, "name": "Waking Stats", "quality": 2}
+enchant_mapping["6625"] = {"id": 200030, "name": "Waking Stats", "quality": 3}
+
 # Weapon
+enchant_mapping["6626"] = {"id": 199967, "name": "Burning Writ", "quality": 1}
+enchant_mapping["6627"] = {"id": 200009, "name": "Burning Writ", "quality": 2}
+enchant_mapping["6628"] = {"id": 200051, "name": "Burning Writ", "quality": 3}
+enchant_mapping["6629"] = {"id": 199966, "name": "Burning Devotion", "quality": 1}
+enchant_mapping["6630"] = {"id": 200008, "name": "Burning Devotion", "quality": 2}
+enchant_mapping["6631"] = {"id": 200050, "name": "Burning Devotion", "quality": 3}
+enchant_mapping["6632"] = {"id": 199969, "name": "Earthen Writ", "quality": 1}
+enchant_mapping["6633"] = {"id": 200011, "name": "Earthen Writ", "quality": 2}
+enchant_mapping["6634"] = {"id": 200053, "name": "Earthen Writ", "quality": 3}
+enchant_mapping["6635"] = {"id": 199968, "name": "Earthen Devotion", "quality": 1}
+enchant_mapping["6636"] = {"id": 200010, "name": "Earthen Devotion", "quality": 2}
+enchant_mapping["6637"] = {"id": 200052, "name": "Earthen Devotion", "quality": 3}
+enchant_mapping["6638"] = {"id": 199971, "name": "Sophic Writ", "quality": 1}
+enchant_mapping["6639"] = {"id": 200013, "name": "Sophic Writ", "quality": 2}
+enchant_mapping["6640"] = {"id": 200055, "name": "Sophic Writ", "quality": 3}
+enchant_mapping["6641"] = {"id": 199970, "name": "Sophic Devotion", "quality": 1}
+enchant_mapping["6642"] = {"id": 200012, "name": "Sophic Devotion", "quality": 2}
+enchant_mapping["6643"] = {"id": 200054, "name": "Sophic Devotion", "quality": 3}
+enchant_mapping["6644"] = {"id": 199973, "name": "Frozen Writ", "quality": 1}
+enchant_mapping["6645"] = {"id": 200015, "name": "Frozen Writ", "quality": 2}
+enchant_mapping["6646"] = {"id": 200057, "name": "Frozen Writ", "quality": 3}
+enchant_mapping["6647"] = {"id": 199972, "name": "Frozen Devotion", "quality": 1}
+enchant_mapping["6648"] = {"id": 200014, "name": "Frozen Devotion", "quality": 2}
+enchant_mapping["6649"] = {"id": 200056, "name": "Frozen Devotion", "quality": 3}
+enchant_mapping["6650"] = {"id": 199975, "name": "Wafting Writ", "quality": 1}
 enchant_mapping["6651"] = {"id": 200017, "name": "Wafting Writ", "quality": 2}
+enchant_mapping["6652"] = {"id": 200059, "name": "Wafting Writ", "quality": 3}
+enchant_mapping["6653"] = {"id": 199974, "name": "Wafting Devotion", "quality": 1}
+enchant_mapping["6654"] = {"id": 200016, "name": "Wafting Devotion", "quality": 2}
+enchant_mapping["6655"] = {"id": 200058, "name": "Wafting Devotion", "quality": 3}
 
 # -----------------------------------------
 # Death Knight Runeforging -- (note this is spell id, since there's no scroll)
-enchant_mapping["3368"] = {"spell_id" : 53344,
-                           "name" : "Fallen Crusader"}
-enchant_mapping["3370"] = {"spell_id" : 53343,
-                           "name" : "Razorice"}
-enchant_mapping["6241"] = {"spell_id" : 326805,
-                           "name" : "Sanguination"}
-enchant_mapping["6244"] = {"spell_id" : 326977,
-                           "name" : "Unending Thirst"}
-enchant_mapping["6242"] = {"spell_id" : 326855,
-                           "name" : "Spellwarding"}
-enchant_mapping["6243"] = {"spell_id" : 326911,
-                           "name" : "Hysteria"}
-enchant_mapping["6245"] = {"spell_id" : 327082,
-                           "name" : "Apocalypse"}
-enchant_mapping["3847"] = {"spell_id" : 62158,
-                           "name" : "Stoneskin Gargoyle"}
+enchant_mapping["3368"] = {"spell_id" : 53344, "name" : "Fallen Crusader"}
+enchant_mapping["3370"] = {"spell_id" : 53343, "name" : "Razorice"}
+enchant_mapping["3847"] = {"spell_id" : 62158, "name" : "Stoneskin Gargoyle"}
+enchant_mapping["6241"] = {"spell_id" : 326805, "name" : "Sanguination"}
+enchant_mapping["6242"] = {"spell_id" : 326855, "name" : "Spellwarding"}
+enchant_mapping["6243"] = {"spell_id" : 326911, "name" : "Hysteria"}
+enchant_mapping["6244"] = {"spell_id" : 326977, "name" : "Unending Thirst"}
+enchant_mapping["6245"] = {"spell_id" : 327082, "name" : "Apocalypse"}
 
 # -----------------------------------------
 # LEGACY
 # Battle for Azeroth Engineering
 ## Eng. Belt Enhancements -- note this is by SPELL ID, since there's no item
-
-enchant_mapping["5953"] = {"spell_id": 255940,
-                           "name": "Personal Space Amplifier"}
-
-enchant_mapping["5967"] = {"spell_id": 269123,
-                           "name": "Miniaturized Plasma Shield"}
-
-enchant_mapping["5952"] = {"spell_id": 255936,
-                           "name": "Holographic Horror Projector"}
+enchant_mapping["5953"] = {"spell_id": 255940,  "name": "Personal Space Amplifier"}
+enchant_mapping["5967"] = {"spell_id": 269123,  "name": "Miniaturized Plasma Shield"}
+enchant_mapping["5952"] = {"spell_id": 255936, "name": "Holographic Horror Projector"}
 
 ## Eng. Belt Enhancements -- note this is by SPELL ID, since there's no item
-enchant_mapping["4223"] = {"spell_id": 55016,
-                           "name": "Nitro Boosts"}
-
-enchant_mapping["4222"] = {"spell_id": 67839,
-                           "name": "Mind Amplification Dish"}
-
-enchant_mapping["4214"] = {"spell_id": 84425,
-                           "name": "Cardboard Assassin"}
-
-enchant_mapping["3599"] = {"spell_id": 54736,
-                           "name": "EMP Generator"}
-
-enchant_mapping["3601"] = {"spell_id": 54793,
-                           "name": "Frag Belt"}
-
-enchant_mapping["4188"] = {"spell_id": 84427,
-                           "name": "Grounded Plasma Shield"}
-
-enchant_mapping["4187"] = {"spell_id": 84424,
-                           "name": "Invisibility Field"}
-
-enchant_mapping["4750"] = {"spell_id": 82200,
-                           "name": "Spinal Healing Injector"}
-
-enchant_mapping["5000"] = {"spell_id": 109099,
-                           "name": "Watergliding Jets"}
-
+enchant_mapping["3599"] = {"spell_id": 54736, "name": "EMP Generator"}
+enchant_mapping["3601"] = {"spell_id": 54793, "name": "Frag Belt"}
+enchant_mapping["4187"] = {"spell_id": 84424, "name": "Invisibility Field"}
+enchant_mapping["4188"] = {"spell_id": 84427, "name": "Grounded Plasma Shield"}
+enchant_mapping["4222"] = {"spell_id": 67839, "name": "Mind Amplification Dish"}
+enchant_mapping["4214"] = {"spell_id": 84425, "name": "Cardboard Assassin"}
+enchant_mapping["4223"] = {"spell_id": 55016, "name": "Nitro Boosts"}
+enchant_mapping["4750"] = {"spell_id": 82200, "name": "Spinal Healing Injector"}
+enchant_mapping["5000"] = {"spell_id": 109099, "name": "Watergliding Jets"}
